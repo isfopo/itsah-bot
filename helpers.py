b3a35cd82e7e3eb5fcfa9e6c27f2451abcc538d5
Isaac Poole <isaacpoolemusic@gmail.com>
2021-05-30 16:04:16 -0400

    makes two list helper functions

--- helpers.py
+++ helpers.py
@@ -1,4 +1,12 @@
-def is_command (msg): # Checks if the message is a command call
+from re import match
+
+def is_command (msg) -> bool: # Checks if the message is a command call
   if len(msg.content) == 0: return False
   elif msg.content.split()[0].startswith('--'): return True
   else: return False
+
+def filter_by_regex(values: list = [], regex: str = "*") -> list:
+  return list(filter(lambda v: match(regex, v), values))
+
+def first_or_none(values: list = []) -> list:
+  return values[0] if len(values) > 0 else []
