405dd76f71b8f484d8406435db0aad218de78d4b
Isaac Poole <isaacpoolemusic@gmail.com>
2021-05-30 16:44:01 -0400

    extracts param by keyword

--- helpers.py
+++ helpers.py
@@ -9,4 +9,13 @@ def filter_by_regex(values: list = [], regex: str = "*") -> list:
   return list(filter(lambda v: match(regex, v), values))
 
 def first_or_none(values: list = []) -> list:
-  return values[0] if len(values) > 0 else []
+  return values[0] if len(values) > 0 else None
+
+def last_or_none(values: list = []) -> list:
+  return values[-1] if len(values) > 0 else None
+
+def extract_param(params: list, keyword: str):
+  param = first_or_none(filter_by_regex(params, f"^{keyword}.*$"))
+  if param:
+    return last_or_none(param.split("="))
+  else: return None
