d51f45db38cf186e4248ce9e3a7d740e4c0875e3
Isaac Poole <isaacpoolemusic@gmail.com>
2021-05-30 13:51:21 -0400

    averages scores of all messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+from numpy import negative
 from sentiment_analysis import get_sentiment
 import helpers
 
@@ -23,20 +24,24 @@ async def on_message(message):
     params = message.content.split()[1:] if len(message.content.split()) > 1 else None #IDEA: use params to get the sentiment from a particular user like "user=isfopo"
   else: return
 
-  if cmd == "hello":
-    await message.channel.send("hello")
-
-  elif cmd == 'scan':
+  if cmd == 'itsah':
+    await message.channel.send("Starting Analysis")
+    message_count = 0
+    overall_score = 0.0
 
     async for msg in message.channel.history(limit=LIMIT):                 
       if msg.content and not msg.author.bot and not helpers.is_command(msg):
-        (score, prediction) = get_sentiment(msg.content, "model_artifacts") # prediction is positive or negative, score is how positive or negative it is
-        
-        print(
-          f"Message: {msg.content}"
-          f"\tPredicted sentiment: {prediction}"
-          f"\tScore: {score}"
-        )
+        (prediction, score) = get_sentiment(msg.content, "model_artifacts")
+        if   prediction == "positive": overall_score += score
+        elif prediction == "negative": overall_score -= score
+        message_count += 1
+
+    if message_count:
+      await message.channel.send(
+        f"\tOverall Score: {overall_score / message_count}"
+      )
+    else:
+      await message.channel.send("There are no messages to analyze!")
 
   
 client.run(os.getenv("DISCORD_TOKEN"))
