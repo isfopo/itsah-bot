69cd4348b65831b06656587e48ca3e0cc9b87cc2
isfopo <55164207+isfopo@users.noreply.github.com>
2021-05-31 23:11:03 -0400

    adds docstrings

--- helpers.py
+++ helpers.py
@@ -1,24 +1,48 @@
 from re import match
 
-def is_command (msg) -> bool: # Checks if the message is a command call
+
+def is_command (msg: str) -> bool:
+  '''
+  Takes a message and determines if it is a command
+  '''
   if len(msg.content) == 0: return False
   elif msg.content.split()[0].startswith('--'): return True
   else: return False
 
+
 def filter_by_regex(values: list = [], regex: str = "*") -> list:
+  '''
+  Filters a list of strings and returns a list only containing strings that match the given regex
+  '''
   return list(filter(lambda v: match(regex, v), values))
 
+
 def first_or_none(values: list = []) -> list:
+  '''
+  Returns the first value of a list, but there is none, returns None
+  '''
   return values[0] if len(values) > 0 else None
 
+
 def last_or_none(values: list = []) -> list:
+  '''
+  Returns the last value of a list, but there is none, returns None
+  '''
   return values[-1] if len(values) > 0 else None
 
+
 def extract_param(params: list, keyword: str):
+  '''
+  Gets the parameter for a given keyword
+  '''
   param = first_or_none(filter_by_regex(params, f"^{keyword}.*$"))
   if param:
     return last_or_none(param.split("="))
   else: return None
 
+
 def to_percent(decimal: int):
+  '''
+  Converts a decimal to a percentage
+  '''
   return int(decimal * 100)
